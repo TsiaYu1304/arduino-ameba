07afb09ca3b43927b59bd86fc0cde4342dcd8877
neo_jou <neo_jou@realtek.com>
2016-01-11 22:51:22 +0800

    Add example TestMySum

---
+++ example/TestMySum/TestMySum.ino
@@ -0,0 +1,25 @@
+#include "libtest.h"
+
+void setup() {
+  //Initialize serial and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  // prints title with ending line break
+  Serial.println("Fabonacci");
+}
+
+static int a=2;
+static int b=3;
+void loop() {
+  int c;
+  c = mysum(a, b);
+  Serial.print(c);
+  Serial.print(" ");
+
+  a=b;
+  b=c;
+  delay(500);
+}
